451d63a04e15d5b4fecbf5b6b36b41ee0facce71
Skywings1605 <skywings.mediacom@gmail.com>
2022-08-20 18:35:05 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { LandingPage } from './Components/LandingPage';
+import { Header } from './Components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <LandingPage/>  
+    </div>
+  );
+}
+
+export default App;
